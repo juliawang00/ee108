608dd9f8c6c756a8c556b9e428a7a098bf44dd78
juliawang00 <juliawang00@gmail.com>
2020-10-02 18:32:03 -0400

    Update n_bit_multiplier_tb.v

--- lab1estarterfiles/n_bit_multiplier_tb.v
+++ lab1estarterfiles/n_bit_multiplier_tb.v
@@ -14,11 +14,17 @@ initial begin
     #100 $display("%03d * %03d = %03d ", in0, in1, out) ;
     // Check all possibilities for two different width multiplier modules of Width >= 3
     
-    in0 = 4'd0001; in1 = 4'd0010;
-    #100 $display("%03d * %03d = %03d ", in0, in1, out) ;
+    in0 = 4'b0001; in1 = 4'b0010;
+  #100 $display("%03b * %03b = %03b ", in0, in1, out) ;
     
-    in0 = 4'd0110; in1 = 4'd1110;
-    #100 $display("%03d * %03d = %03d ", in0, in1, out) ;
+    in0 = 4'b0110; in1 = 4'b1110;
+  #100 $display("%b * %b = %b ", in0, in1, out) ;
+  
+      in0 = 4'b0110; in1 = 4'b1000;
+  #100 $display("%03b * %03b = %03b ", in0, in1, out) ;
+    
+    in0 = 4'b0011; in1 = 4'b1010;
+  #100 $display("%b * %b = %b ", in0, in1, out) ;
 
 end
 
