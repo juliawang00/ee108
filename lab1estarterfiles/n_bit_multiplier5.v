71e753c5c9b74ec42a780de4814a6dbdbf49280f
juliawang00 <juliawang00@gmail.com>
2020-10-02 18:43:52 -0400

    Update n_bit_multiplier5.v

--- lab1estarterfiles/n_bit_multiplier5.v
+++ lab1estarterfiles/n_bit_multiplier5.v
@@ -1,4 +1,4 @@
-module n_bit_multiplier #(parameter N = 4)
+module n_bit_multiplier #(parameter N=5)
 (
 	input wire [N-1:0] a, // Input 1
 	input wire [N-1:0] b, // Input 2
@@ -44,42 +44,4 @@ module n_bit_multiplier #(parameter N = 4)
   assign p[(2*N)-1:N-1] = {adder_carry[N-1], adder_output[N-1]};
 		
  
-endmodule
-
-module n_bit_adder #(parameter N=4)(
-    input [N-1:0] a, b,
-    input cin,
-    output [N-1:0] out,
-    output of
-    );
-    
-
-    wire [N:0] carry;
-    
-    assign carry[0]=cin;
-    assign of = carry[N];
-        
-    generate
-        genvar i;
-        for (i = 0; i < N; i=i+1) begin : full_adder_array
-            one_bit_full_adder adder_i (
-                .a(a[i]), 
-                .b(b[i]), 
-                .cin(carry[i]),
-                .cout(carry[i+1]), 
-                .s(out[i])
-            );
-        end
-    endgenerate
-endmodule
-
-
-module one_bit_full_adder(
-    input a, b, cin,
-    output wire cout, s // carry and sum
-    );
-    
-    assign s = a ^ b ^ cin ;
-    assign cout = (a & b)|(a & cin)|(b & cin) ; // majority
-
 endmodule
