fc180625ab0b842dc08ca65d9fbe4a8f8e0190b0
juliawang00 <juliawang00@gmail.com>
2020-10-02 18:44:13 -0400

    Update n_bit_multiplier_tb5.v

--- lab1estarterfiles/n_bit_multiplier_tb5.v
+++ lab1estarterfiles/n_bit_multiplier_tb5.v
@@ -2,28 +2,28 @@
 
 module n_bit_multiplier_tb ();
 
-reg [3:0] in0, in1 ;
+  reg [4:0] in0, in1 ;
 
-wire [7:0] out;
+  wire [9:0] out;
 
 
-n_bit_multiplier #(4) DUT(.a(in0), .b(in1), .p(out));
+  n_bit_multiplier #(5) DUT(.a(in0), .b(in1), .p(out));
 
 initial begin
-    in0 = 4'd0; in1 = 4'd0;
+    in0 = 5'd0; in1 = 5'd0;
     #100 $display("%03d * %03d = %03d ", in0, in1, out) ;
     // Check all possibilities for two different width multiplier modules of Width >= 3
     
-    in0 = 4'b0001; in1 = 4'b0010;
+    in0 = 5'b00001; in1 = 5'b10010;
   #100 $display("%03b * %03b = %03b ", in0, in1, out) ;
     
-    in0 = 4'b0110; in1 = 4'b1110;
+    in0 = 5'b10110; in1 = 5'b01110;
   #100 $display("%b * %b = %b ", in0, in1, out) ;
   
-      in0 = 4'b0110; in1 = 4'b1000;
+      in0 = 5'b00110; in1 = 5'b11000;
   #100 $display("%03b * %03b = %03b ", in0, in1, out) ;
     
-    in0 = 4'b0011; in1 = 4'b1010;
+    in0 = 5'b10011; in1 = 5'b01010;
   #100 $display("%b * %b = %b ", in0, in1, out) ;
 
 end
