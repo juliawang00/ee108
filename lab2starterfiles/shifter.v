3d4c3ef4f9598f1ccf28c81a26dc63248f961c56
juliawang00 <juliawang00@gmail.com>
2020-10-08 19:49:27 -0400

    Update shifter.v

--- lab2starterfiles/shifter.v
+++ lab2starterfiles/shifter.v
@@ -1,9 +1,20 @@
+// Shifts the bits of in by distance to the
+// left if direction = 0, right if direction = 1
 module shifter (
-
+  input [4:0] in,
+  input [2:0] distance,
+  input direction,
+  output [4:0] out
 );
-
-// your implementation goes here!
-
-
+  out = 5{0};
+  int shift_size = out.size - distance
+  
+  if(direction == 0) begin
+    out[4:shift_size] = in[shift_size:0];
+  end
+  
+  else if(direction == 1) begin
+    out[shift_size:0] = in[4:shift_size];
+  end
 
 endmodule
