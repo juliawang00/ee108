4fc45ce9a34fe21a81b2933242cb957944fe7930
TristanAbarca <52977898+TristanAbarca@users.noreply.github.com>
2020-10-08 18:45:34 -0600

    Update adder_tb.v

--- lab2starterfiles/adder_tb.v
+++ lab2starterfiles/adder_tb.v
@@ -1,9 +1,40 @@
-module adder_tb (
+module adder_tb ();
+  
+  reg[4:0] in1;
+  reg[4:0] in2;
 
-);
+  wire[4:0] out;
+  wire cout;
 
-// your implementation goes here!
+  adder #() DUT(.in1(in1), .in2(in2), .sum(out), .cout(cout));
+
+ initial begin
+    in1 = 5'd25; 
+  	in2 = 5'd24;
+  
+    #100 $display("%03d + %03d = %03d with %03d", in1, in2, out, cout);
+
+    in1 = 5'd1; 
+    in2 = 5'd2;
+
+    #100 $display("%03d + %03d = %03d with %03d", in1, in2, out, cout);
+
+    in1 = 5'd31; 
+    in2 = 5'd2;
+
+    #100 $display("%03d + %03d = %03d with %03d", in1, in2, out, cout);
+
+    in1 = 5'd1; 
+    in2 = 5'd0;
+
+    #100 $display("%03d + %03d = %03d with %03d", in1, in2, out, cout);
+
+    in1 = 5'd0; 
+    in2 = 5'd0;
+
+    #100 $display("%03d + %03d = %03d with %03d", in1, in2, out, cout);
 
 
+  end
 
 endmodule
