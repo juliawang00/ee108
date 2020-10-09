a7fee2404a506b164ed4f55ac028d0a8145da537
Troy Lawrence <35078371+tlaw72@users.noreply.github.com>
2020-10-08 20:00:40 -0700

    Added comments and test cases

--- lab2starterfiles/float_add_tb.v
+++ lab2starterfiles/float_add_tb.v
@@ -10,46 +10,60 @@ module float_add_tb ();
   float_add #() DUT(.aIn(aIn), .bIn(bIn), .result(out));
   
    initial begin
+     
+     // Test Case 1: Exponents 0, no carry
     aIn = 8'b00001000; 
   	bIn = 8'b00000011;
   
      #100 $display("%b + %b = %b", aIn, bIn, out);
      
+     // Test Case 2: Exponent one, no carry
     aIn = 8'b00110001; 
   	bIn = 8'b00001100;
   
      #100 $display("%b + %b = %b", aIn, bIn, out);
      
+     // Test Case 3: High Exponents, with carry
     aIn = 8'b10010010; 
-  	bIn = 8'b01011111;
+  	bIn = 8'b01111111;
   
      #100 $display("%b + %b = %b", aIn, bIn, out);
      
+     // Test Case 4: Saturation
     aIn = 8'b11111110; 
   	bIn = 8'b11111000;
   
      #100 $display("%b + %b = %b", aIn, bIn, out);
      
+     // Test Case 5: Saturation
     aIn = 8'b11111110; 
   	bIn = 8'b11111110;
   
      #100 $display("%b + %b = %b", aIn, bIn, out);
      
+     //Test Case 6: Adding zeros
     aIn = 8'b00000000; 
   	bIn = 8'b00000000;
   
      #100 $display("%b + %b = %b", aIn, bIn, out);
      
+     // Test Case 7: Adding with zero
     aIn = 8'b00000000; 
   	bIn = 8'b00010000;
   
      #100 $display("%b + %b = %b", aIn, bIn, out);
      
+     // Test Case 8: Adding the extremities
     aIn = 8'b00000000; 
   	bIn = 8'b11111111;
   
      #100 $display("%b + %b = %b", aIn, bIn, out);
      
+     //Test Case 9: Adding to get highest representable number without saturation
+     aIn = 8'b11011111; 
+  	 bIn = 8'b11011111;
+  
+     #100 $display("%b + %b = %b", aIn, bIn, out);
    end
 
 
