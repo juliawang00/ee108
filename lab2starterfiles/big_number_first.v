9e3165a5db05ffae353d6ce818fcf0f805c73648
Troy Lawrence <35078371+tlaw72@users.noreply.github.com>
2020-10-08 17:59:53 -0700

    Working big_number_first.v

--- lab2starterfiles/big_number_first.v
+++ lab2starterfiles/big_number_first.v
@@ -1,9 +1,23 @@
 module big_number_first (
+  input wire [7:0] aIn,
+  input wire [7:0] bIn,
+  output reg [7:0] aOut,
+  output reg [7:0] bOut
 
 );
-
-// your implementation goes here!
-
-
+  
+  wire [2:0] aExp = aIn >> 5;
+  wire [2:0] bExp = bIn >> 5;
+  
+  always @(*) begin
+    if (aIn > bIn) begin
+      aOut = aIn;
+      bOut = bIn;
+    end
+    else begin
+      aOut = bIn;
+      bOut = aIn;
+    end
+  end
 
 endmodule
