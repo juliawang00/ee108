a023f6380530c50c31b3813c3c7a495b0ab54dc7
juliawang00 <juliawang00@gmail.com>
2020-10-08 20:40:31 -0400

    Update shifter_tb.v

--- lab2starterfiles/shifter_tb.v
+++ lab2starterfiles/shifter_tb.v
@@ -11,12 +11,28 @@ module shifter_tb (
   
   initial begin
     // Test Case 1
-    i = 5'b00100;
+    i = 5'b01000;
     d = 3'b001;
     di = 1'b1;
     #5
     $display("Input is %b, we are shifting %b", i, d);
-    $display("Output is %b, we expected %b", o, 5'b10000);
+    $display("Output is %b, we expected %b", o, 5'b00100);
+    
+    // Test Case 2
+    i = 5'b01101;
+    d = 3'b010;
+    di = 1'b1;
+    #5
+    $display("Input is %b, we are shifting %b", i, d);
+    $display("Output is %b, we expected %b", o, 5'b00011);
+    
+    // Test Case 3
+    i = 5'b00101;
+    d = 3'b011;
+    di = 1'b1;
+    #5
+    $display("Input is %b, we are shifting %b", i, d);
+    $display("Output is %b, we expected %b", o, 5'b01000);
   end
 
 
