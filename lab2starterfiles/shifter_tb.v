8c02c5e17f7659edc45345f83259e3b1503cbd15
Troy Lawrence <35078371+tlaw72@users.noreply.github.com>
2020-10-08 19:40:39 -0700

    Added a few cases and added comments

--- lab2starterfiles/shifter_tb.v
+++ lab2starterfiles/shifter_tb.v
@@ -10,7 +10,7 @@ module shifter_tb (
   shifter dut(.in(i), .distance(d), .direction(di), .out(o));
   
   initial begin
-    // Test Case 1
+    // Test Case 1: Bit shifting right once
     i = 5'b01000;
     d = 3'b001;
     di = 1'b1;
@@ -18,15 +18,23 @@ module shifter_tb (
     $display("Input is %b, we are shifting %b towards %b", i, d, di);
     $display("Output is %b, we expected %b", o, 5'b00100);
     
-    // Test Case 2
+    // Test Case 2: Bit shifting right five times -> all zeros
     i = 5'b01101;
-    d = 3'b010;
+    d = 3'b111;
     di = 1'b1;
     #5
     $display("Input is %b, we are shifting %b towards %b", i, d, di);
     $display("Output is %b, we expected %b", o, 5'b00011);
     
-    // Test Case 3
+    // Test Case 3: Bit shifting left zero times -> i is unchanged
+    i = 5'b11111;
+    d = 3'b0;
+    di = 1'b1;
+    #5
+    $display("Input is %b, we are shifting %b towards %b", i, d, di);
+    $display("Output is %b, we expected %b", o, 5'b00000);
+    
+    // Test Case 4: Bit shifting left three times
     i = 5'b00101;
     d = 3'b011;
     di = 1'b0;
@@ -34,7 +42,15 @@ module shifter_tb (
     $display("Input is %b, we are shifting %b towards %b", i, d, di);
     $display("Output is %b, we expected %b", o, 5'b01000);
     
-    // Test Case 4
+    // Test Case 5: Bit shifting left zero times -> i is unchanged
+    i = 5'b11111;
+    d = 3'b0;
+    di = 1'b0;
+    #5
+    $display("Input is %b, we are shifting %b towards %b", i, d, di);
+    $display("Output is %b, we expected %b", o, 5'b00000);
+    
+    // Test Case 6: Bit shifting left five times -> all zeros
     i = 5'b11111;
     d = 3'b111;
     di = 1'b0;
