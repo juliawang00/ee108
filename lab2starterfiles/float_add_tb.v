8273852e401d9986d9f2e1bca72ff5d71ec41c6a
TristanAbarca <52977898+TristanAbarca@users.noreply.github.com>
2020-10-08 19:29:36 -0600

    Update float_add_tb.v

--- lab2starterfiles/float_add_tb.v
+++ lab2starterfiles/float_add_tb.v
@@ -1,8 +1,36 @@
-module float_add_tb (
-
-);
+module float_add_tb ();
 
 // your implementation goes here!
+  reg [7:0] aIn;
+  reg [7:0] bIn;
+  
+  wire[7:0] out;
+  
+  
+  float_add #() DUT(.aIn(aIn), .bIn(bIn), .result(out));
+  
+   initial begin
+    aIn = 8'b00001000; 
+  	bIn = 8'b00000011;
+  
+     #100 $display("%03d + %03d = %03d with %03d", aIn, bIn, cout);
+     
+    aIn = 8'b00110001; 
+  	bIn = 8'b00001100;
+  
+     #100 $display("%03d + %03d = %03d with %03d", aIn, bIn, cout);
+     
+    aIn = 8'b10010010; 
+  	bIn = 8'b01011111;
+  
+     #100 $display("%03d + %03d = %03d with %03d", aIn, bIn, cout);
+     
+    aIn = 8'b11111110; 
+  	bIn = 8'b11111000;
+  
+     #100 $display("%03d + %03d = %03d with %03d", aIn, bIn, cout);
+     
+   end
 
 
 
