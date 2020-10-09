5b9bb7b4b719d5c74bd2351c876f348a3e34ae34
juliawang00 <juliawang00@gmail.com>
2020-10-08 21:50:05 -0400

    Update float_add_tb.v

--- lab2starterfiles/float_add_tb.v
+++ lab2starterfiles/float_add_tb.v
@@ -13,22 +13,22 @@ module float_add_tb ();
     aIn = 8'b00001000; 
   	bIn = 8'b00000011;
   
-     #100 $display("%03d + %03d = %03d with %03d", aIn, bIn, cout);
+     #100 $display("%b + %b = %b", aIn, bIn, out);
      
     aIn = 8'b00110001; 
   	bIn = 8'b00001100;
   
-     #100 $display("%03d + %03d = %03d with %03d", aIn, bIn, cout);
+     #100 $display("%b + %b = %b", aIn, bIn, out);
      
     aIn = 8'b10010010; 
   	bIn = 8'b01011111;
   
-     #100 $display("%03d + %03d = %03d with %03d", aIn, bIn, cout);
+     #100 $display("%b + %b = %b", aIn, bIn, out);
      
     aIn = 8'b11111110; 
   	bIn = 8'b11111000;
   
-     #100 $display("%03d + %03d = %03d with %03d", aIn, bIn, cout);
+     #100 $display("%b + %b = %b", aIn, bIn, out);
      
    end
 
