cedd73aeed243aa87d6e3a1774eb05e4985e17d2
TristanAbarca <52977898+TristanAbarca@users.noreply.github.com>
2020-10-08 18:46:08 -0600

    Update adder.v

--- lab2starterfiles/adder.v
+++ lab2starterfiles/adder.v
@@ -1,9 +1,14 @@
-module adder (
-
+module adder(
+  input wire[4:0] in1,
+  input wire[4:0] in2,
+  output wire[4:0] sum,
+  output wire cout
 );
-
-// your implementation goes here!
-
-
-
-endmodule
+  
+  wire[5:0] added;
+  assign added = in1 + in2;
+  
+  assign sum[4:0] = added[4:0];
+  assign cout = added[5];  
+  
+endmodule 
