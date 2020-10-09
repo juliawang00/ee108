0e5f95f3dfb481dae80d0a8ed7125f5a1b7fdb99
juliawang00 <juliawang00@gmail.com>
2020-10-08 20:37:26 -0400

    Update shifter_tb.v

--- lab2starterfiles/shifter_tb.v
+++ lab2starterfiles/shifter_tb.v
@@ -2,7 +2,22 @@ module shifter_tb (
 
 );
 
-// your implementation goes here!
+  reg [4:0] i;
+  reg [2:0] d;
+  reg di;
+  wire [4:0] o;
+
+  shifter dut(.in(i), .distance(d), .direction(di), .out(o));
+  
+  initial begin
+    // Test Case 1
+    i = 5'b00100;
+    d = 3'b001;
+    di = 1'b1;
+    #5
+    $display("Input is %b, we are shifting %b", i, d);
+    $display("Output is %b, we expected %b", o, 5'b10000);
+  end
 
 
 
