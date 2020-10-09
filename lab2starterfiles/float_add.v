82253c0eabb23d0d92315db715f3cc97d520f45a
juliawang00 <juliawang00@gmail.com>
2020-10-08 21:49:44 -0400

    Update float_add.v

--- lab2starterfiles/float_add.v
+++ lab2starterfiles/float_add.v
@@ -3,19 +3,23 @@ module float_add (
     input wire [7:0] bIn,
     output wire [7:0] result
 );
-    reg [7:0] aOut; // contains the floating point w larger exp
-    reg [7:0] bOut; // contains floating point w smaller exp
+    wire [7:0] aOut; // contains the floating point w larger exp
+    wire [7:0] bOut; // contains floating point w smaller exp
     big_number_first #() bigger_exp (
         .aIn(aIn), .bIn(bIn), .aOut(aOut), .bOut(bOut)
 	);
     
     // find difference in exponents
-    reg [2:0] exp_1 = aOut[7:5];
-    reg [2:0] exp_2 = bOut[7:5];
-    reg [2:0] diff = exp_1 - exp_2;
+  wire [2:0] exp_1;
+  assign exp_1 = aOut[7:5];
+  wire [2:0] exp_2;
+  assign exp_2 = bOut[7:5];
+  wire [2:0] diff;
+  assign diff = exp_1 - exp_2;
     
     // shift bOut's mantissa right by the difference
-    reg [4:0] mantissa = bOut[4:0];
+  wire [4:0] mantissa;
+  assign mantissa = bOut[4:0];
     wire [4:0] shifted_mantissa;
     
     shifter #() s (
@@ -23,21 +27,26 @@ module float_add (
     );
     
     // add aOut and bOut shifted mantissas together
-    reg [4:0] a_mantissa = aOut[4:0];
+  wire [4:0] a_mantissa;
+  assign a_mantissa = aOut[4:0];
     wire [4:0] sum;
     wire carry;
     adder #() add (
-        .a(a_mantissa), .b(shifted_mantissa), .sum(sum), .cout(carry)
+      .in1(a_mantissa), .in2(shifted_mantissa), .sum(sum), .cout(carry)
     );
     
     // deal with the carry
+  reg [4:0] assign_sum;
     always @* begin
         if(carry == 1'b1) begin
-            sum = {1'b1, sum[4:1]};
+            assign_sum = {1'b1, sum[4:1]};
         end
+      else begin
+        assign_sum = sum;
+      end
     end
     
-    assign result = {exp_1, sum};
+  assign result = {exp_1, assign_sum};
     
 
 endmodule
