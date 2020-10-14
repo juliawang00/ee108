a8614772751c2a18be23684c91041810968bfbd5
Troy Lawrence <35078371+tlaw72@users.noreply.github.com>
2020-10-14 08:21:28 +0200

    beat32 First Attempt.
    Not tested. Should continuously count down from 3125000 and only output done at 0.

--- lab3starterfiles/beat32.v
+++ lab3starterfiles/beat32.v
@@ -1,6 +1,34 @@
-module beat32 (
-
-
+module beat32 #(parameter n = 22)
+  ( input clk, 
+//     input reset, 	// Standard system clock and reset
+    input in, 	// Value to be loaded into countdown: 22'd3125000
+    output wire done	// 
 );
+  reg [n-1:0] next;
+  wire [n-1:0] nextstate;
+  
+  DFF #(n) count(clk, next, nextstate);
+  
+  
+  always@(*) begin
+    if (!(|next)) begin
+      next = in;
+    end
+    else if((|next)) begin
+      next = nextstate - 1'b1;
+    end
+    else begin
+      next = nextstate;
+    end
+  end
+  
+//   always @(*) begin
+//     case(out)
+//       22'b0: next = in;
+//       1'b0: next = out + 22'b1;      
+//     endcase
+//   end
+  
+  assign done = !(|next);
 
 endmodule
