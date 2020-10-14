89cf9db895a047aa2ba4f556c7f60ca91b0cb780
TristanAbarca <52977898+TristanAbarca@users.noreply.github.com>
2020-10-14 01:23:45 -0600

    Update shifter.v

--- lab3starterfiles/shifter.v
+++ lab3starterfiles/shifter.v
@@ -1,5 +1,35 @@
-module shifter (
-
+module shifter ( 
+  input wire shift_right, 
+  input wire [3:0] state, 
+  output wire [3:0] out
 );
 
+  reg [3:0] shifted;
+  
+  always @* begin
+    if(shift_right) begin
+      
+      if(state[0] != 1)begin
+      	shifted = state >> 1;
+      end
+      
+      else begin
+        shifted = state;
+      end 
+      
+    end
+
+    else begin
+      if(state[3] != 1'b1) begin
+      	shifted = state << 1;
+      end
+      else begin
+        shifted = state;
+      end
+    end
+  end
+  
+  assign out = shifted;
+  
+  
 endmodule
