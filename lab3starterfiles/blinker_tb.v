e06c2b9cf39e1d0f90930f96623d3731c5f8182a
juliawang00 <juliawang00@gmail.com>
2020-10-14 17:58:59 -0400

    Update blinker_tb.v

--- lab3starterfiles/blinker_tb.v
+++ lab3starterfiles/blinker_tb.v
@@ -1,3 +1,44 @@
 module blinker_tb ();
+  
+  reg clk, switch, reset, in, out;
+  
+  blinker #() DUT(.clk(clk), .switch(switch), .reset(reset), .out(out));
+  
+  intial
+  forever
+   begin
+     #5 clk = 1 ; 
+     #5 clk = 0 ;
+   end
+  
+ initial begin
 
+   #10 reset = 0; // start without reset (x state)
+   #20 reset = 1;
+   #10 reset = 0; // remove reset
+   
+   // switch on, input 1
+   #50 switch = 1'b1;
+   in = 1'b1;
+   #100 $display("Switch is %b, in is %b, output is %b", switch, in, out);
+   
+   // switch on, input 0
+   #50 switch = 1'b1;
+   in = 1'b0;
+   #100 $display("Switch is %b, in is %b, output is %b", switch, in, out);
+   
+   // switch off, input 1
+   #50 switch = 1'b0;
+   in = 1'b1;
+   #100 $display("Switch is %b, in is %b, output is %b", switch, in, out);
+   
+   // switch off, input 0
+   #50 switch = 1'b0;
+   in = 1'b0;
+   #100 $display("Switch is %b, in is %b, output is %b", switch, in, out);
+   
+   $stop
+   
+ end 
+endmodule 
 endmodule
