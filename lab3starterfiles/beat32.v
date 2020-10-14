6df2b7f2d13c96c164dca353813867e648b8b597
Troy Lawrence <35078371+tlaw72@users.noreply.github.com>
2020-10-15 01:29:38 +0200

    Made DFF a DFFR

--- lab3starterfiles/beat32.v
+++ lab3starterfiles/beat32.v
@@ -1,12 +1,11 @@
 module beat32 #(parameter n = 22)
-  ( input clk, 
-    input load, 	// Value to be loaded into countdown: 22'd3125000
+  ( input clk, rst, load, 	// Clock, Reset, Value to be loaded into countdown: 22'd3125000
     output wire done	// Output high when counter reaches zero
 );
   wire [n-1:0] next;
   reg [n-1:0] nextState;
   
-  dff #(n) count(clk, nextState, next);
+  dffr #(n) count(clk, rst, nextState, next);
   
   // Counter: Decrement unless next is all zeros. Then set to load.
   always @(*) begin
@@ -25,3 +24,33 @@ module beat32 #(parameter n = 22)
   assign done = !(|next);
 
 endmodule
+
+
+// module beat32 #(parameter n = 22)
+//   ( input clk, 
+//     input load, 	// Value to be loaded into countdown: 22'd3125000
+//     output wire done	// Output high when counter reaches zero
+// );
+//   wire [n-1:0] next, nextState;
+  
+//   dff #(n) count(clk, nextState, next);
+  
+//   Mux2 #(n) mux(next - 1'b1, load, {(|next), !(|next)}, nextState)
+  
+// //   // Counter: Decrement unless next is all zeros. Then set to load.
+// //   always @(*) begin
+// //     if (!(|next)) begin
+// //       nextState = load;
+// //     end
+// //     else if((|next)) begin
+// //       nextState = next - 1'b1;
+// //     end
+// //     else begin
+// //        nextState = next;
+// //     end
+// //   end
+  
+//   // Output high when counter reaches zero
+//   assign done = !(|next);
+
+// endmodule
