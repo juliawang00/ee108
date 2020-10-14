bf7d3e50c91288b77d1bb939b129e9ef5e031bf6
TristanAbarca <52977898+TristanAbarca@users.noreply.github.com>
2020-10-14 00:35:26 -0600

    Update timer.v

--- lab3starterfiles/timer.v
+++ lab3starterfiles/timer.v
@@ -1,6 +1,16 @@
+// Code your design here
 module timer (
-
+  input clk, 
+  input wire [3:0] load_value, 
+  input wire count_en,
+  output wire q
 );
-
+  
+  dffre #() ff(.clk (clk),
+               .r (0), 
+               .en(count_en),
+               .d (1), 
+               .q(q)
+              );
 
 endmodule
