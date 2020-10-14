491d067e7c0caebc0e6d33330a28968c01de6fe1
Troy Lawrence <35078371+tlaw72@users.noreply.github.com>
2020-10-15 00:55:08 +0200

    Blinker running tb. First output is zero tho

--- lab3starterfiles/blinker.v
+++ lab3starterfiles/blinker.v
@@ -1,10 +1,12 @@
 module blinker (
     input clk, // The clk input
     input switch, // The enabler is the switch output from timer
+    //input in,
     input reset, 	// Standard system clock and reset
-    input in, 	// Initialized on (1'b1)
-    output out	// 
+    output wire out	// 
 );
+  
+  reg in;
   
   // Switch is fed from timer, in should be intialized at one to start the blinker on.
   dffre #(1) blink(clk, reset, switch, in, out);
