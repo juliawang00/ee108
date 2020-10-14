1928233a4b88b5100b137fa2fd84ee1ebf4010fa
Troy Lawrence <35078371+tlaw72@users.noreply.github.com>
2020-10-14 21:20:54 +0200

    Fixed blinker to have switch as enabler, not clock input.

--- lab3starterfiles/blinker.v
+++ lab3starterfiles/blinker.v
@@ -1,18 +1,19 @@
 module blinker (
-    input switch, // The clk input is the switch output from timer
+    input clk, // The clk input
+    input switch, // The enabler is the switch output from timer
     input reset, 	// Standard system clock and reset
     input in, 	// Initialized on (1'b1)
     output out	// 
 );
   
   // Switch is fed from timer, in should be intialized at one to start the blinker on.
-  dff #(1) blink(switch, in, out);
+  dffre #(1) blink(clk, reset, switch, in, out);
   
   always @(*) begin
     case(out) 
       1'b0: in = 1'b1;
       1'b1: in = 1'b0;
-      default: in = 1'b1;
+      default: in = out;
     endcase
   end
 
