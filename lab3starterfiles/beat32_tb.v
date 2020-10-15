e79af29c62fa984e9854312c1a107663771f3ab6
juliawang00 <juliawang00@gmail.com>
2020-10-14 20:45:29 -0400

    Update beat32_tb.v

--- lab3starterfiles/beat32_tb.v
+++ lab3starterfiles/beat32_tb.v
@@ -1,4 +1,84 @@
 module beat32_tb ();
-
+  reg clk;
+  reg [21:0] load;
+  wire done;
+  
+  beat32 #() DUT(.clk(clk), .load(load), .done(done));
+  initial
+    forever begin
+      #5 clk = 1;
+      #5 clk = 0;
+    end
+  
+  initial begin
+    load = 22'd3125000;
+    $display(load);
+    #10
+    $display("The output has gone to %b", done);
+    #10
+    $display("The output has gone to %b", done);
+    #10
+    $display("The output has gone to %b", done);
+    #10
+    $display("The output has gone to %b", done);
+    #10
+    $display("The output has gone to %b", done);
+    #10
+    $display("The output has gone to %b", done);
+    #10
+    $display("The output has gone to %b", done);
+    #10
+    $display("The output has gone to %b", done);
+    #10
+    $display("The output has gone to %b", done);
+    #10
+    $display("The output has gone to %b", done);
+    #10
+    $display("The output has gone to %b", done);
+    #10
+    $display("The output has gone to %b", done);
+    #10
+    $display("The output has gone to %b", done);
+    #10
+    $display("The output has gone to %b", done);
+    #10
+    $display("The output has gone to %b", done);
+    #10
+    $display("The output has gone to %b", done);
+    #10
+    $display("The output has gone to %b", done);
+    #10
+    $display("The output has gone to %b", done);
+    #10
+    $display("The output has gone to %b", done);
+    #10
+    $display("The output has gone to %b", done);
+    #10
+    $display("The output has gone to %b", done);
+    #10
+    $display("The output has gone to %b", done);
+    #10
+    $display("The output has gone to %b", done);
+    #10
+    $display("The output has gone to %b", done);
+    #10
+    $display("The output has gone to %b", done);
+    #10
+    $display("The output has gone to %b", done);
+    #10
+    $display("The output has gone to %b", done);
+    #10
+    $display("The output has gone to %b", done);
+    #10
+    $display("The output has gone to %b", done);
+    #10
+    $display("The output has gone to %b", done);
+    #10
+    $display("The output has gone to %b", done);
+    #10
+    $display("The output has gone to %b", done);
+    $stop;
+    
+  end
 
 endmodule
