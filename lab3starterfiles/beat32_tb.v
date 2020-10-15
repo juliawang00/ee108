680feccf3ea242c583cc92ef630c62f0507a1265
Troy Lawrence <35078371+tlaw72@users.noreply.github.com>
2020-10-15 05:07:33 +0200

    Reset included

--- lab3starterfiles/beat32_tb.v
+++ lab3starterfiles/beat32_tb.v
@@ -1,9 +1,13 @@
+// Code your testbench here
+// or browse Examples
 module beat32_tb ();
-  reg clk;
+  reg clk, reset;
   reg [21:0] load;
   wire done;
   
-  beat32 #() DUT(.clk(clk), .load(load), .done(done));
+  
+  
+  beat32 #() DUT(.clk(clk), .rst(reset), .load(load), .done(done));
   initial
     forever begin
       #5 clk = 1;
@@ -13,6 +17,13 @@ module beat32_tb ();
   initial begin
     load = 22'd3125000;
     $display(load);
+    #10 reset = 0; // start without reset (x state)
+    #10 $display("First Reset: out %b", done);
+    #20 reset = 1;
+    #10 $display("Second Reset: out %b", done);
+    #10 reset = 0; // remove reset  
+    
+    #10 $display("Third Reset: out %b", done);
     #10
     $display("The output has gone to %b", done);
     #10
