c2a3a366f582fe389a51285ea954e5417c563b82
juliawang00 <juliawang00@gmail.com>
2020-10-15 01:25:19 -0400

    Update programmable_blinker_tb.v

--- lab3starterfiles/programmable_blinker_tb.v
+++ lab3starterfiles/programmable_blinker_tb.v
@@ -1,8 +1,8 @@
 module programmable_blinker_tb ();
   reg up_button, down_button, clk, rst;
-  wire output;
+  wire out;
   
-  programmable_blinker #() DUT(.shift_left(up_button), .shift_right(down_button), .clk(clk), .rst(rst), .rear_light(output));
+  programmable_blinker #() DUT(.shift_left(up_button), .shift_right(down_button), .clk(clk), .rst(rst), .rear_light(out));
   
   initial
   forever begin
