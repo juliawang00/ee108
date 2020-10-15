0c005367af2848e5772a864a65321ae388cb2b4f
Troy Lawrence <35078371+tlaw72@users.noreply.github.com>
2020-10-15 05:58:06 +0200

    Final Testbench

--- lab3starterfiles/beat32_tb.v
+++ lab3starterfiles/beat32_tb.v
@@ -1,93 +1,91 @@
-// Code your testbench here
-// or browse Examples
 module beat32_tb ();
   reg clk, reset;
   reg [21:0] load;
-  wire done;
+  wire [21:0]done;
   
   
-  
+  // Initiate beat module
   beat32 #() DUT(.clk(clk), .rst(reset), .load(load), .done(done));
+  
+  // Initiate clock. Set to 10 unit intervals for testing purposes (3125000 cycles is too much)
   initial
     forever begin
       #5 clk = 1;
       #5 clk = 0;
     end
   
+  // Initiate by resetting the clock and beginning countdown. There should be an output of one every ten cycles.
   initial begin
-    load = 22'd3125000;
-    $display(load);
+    load = 22'd10;
+    $display("Load value: %d", load);
+    
     #10 reset = 0; // start without reset (x state)
-    #10 $display("First Reset: out %b", done);
-    #20 reset = 1;
-    #10 $display("Second Reset: out %b", done);
+    #10 $display("Reset at Zero, done should be high next: done %d", done);
+    #10 reset = 1;
+    #10 $display("Reset at One, Load is set: done %d", done);
     #10 reset = 0; // remove reset  
     
-    #10 $display("Third Reset: out %b", done);
+    #10 $display("2: Reset back at zero: done %d", done);
     #10
-    $display("The output has gone to %b", done);
+    $display("3: The output has gone to %d", done);
     #10
-    $display("The output has gone to %b", done);
+    $display("4: The output has gone to %d", done);
     #10
-    $display("The output has gone to %b", done);
+    $display("5: The output has gone to %d", done);
     #10
-    $display("The output has gone to %b", done);
+    $display("6: The output has gone to %d", done);
     #10
-    $display("The output has gone to %b", done);
+    $display("7: The output has gone to %d", done);
     #10
-    $display("The output has gone to %b", done);
+    $display("8: The output has gone to %d", done);
     #10
-    $display("The output has gone to %b", done);
+    $display("9: The output has gone to %d", done);
     #10
-    $display("The output has gone to %b", done);
+    $display("10: The output has gone to %d", done);
     #10
-    $display("The output has gone to %b", done);
+    $display("11: The output has gone to %d", done);
     #10
-    $display("The output has gone to %b", done);
+    $display("12: The output has gone to %d", done);
     #10
-    $display("The output has gone to %b", done);
+    $display("13: The output has gone to %d", done);
     #10
-    $display("The output has gone to %b", done);
+    $display("14: The output has gone to %d", done);
     #10
-    $display("The output has gone to %b", done);
+    $display("15: The output has gone to %d", done);
     #10
-    $display("The output has gone to %b", done);
+    $display("16: The output has gone to %d", done);
     #10
-    $display("The output has gone to %b", done);
+    $display("17: The output has gone to %d", done);
     #10
-    $display("The output has gone to %b", done);
+    $display("18: The output has gone to %d", done);
     #10
-    $display("The output has gone to %b", done);
+    $display("19: The output has gone to %d", done);
     #10
-    $display("The output has gone to %b", done);
+    $display("20: The output has gone to %d", done);
     #10
-    $display("The output has gone to %b", done);
+    $display("21: The output has gone to %d", done);
     #10
-    $display("The output has gone to %b", done);
+    $display("22: The output has gone to %d", done);
     #10
-    $display("The output has gone to %b", done);
+    $display("23: The output has gone to %d", done);
     #10
-    $display("The output has gone to %b", done);
+    $display("24: The output has gone to %d", done);
     #10
-    $display("The output has gone to %b", done);
+    $display("25: The output has gone to %d", done);
     #10
-    $display("The output has gone to %b", done);
+    $display("26: The output has gone to %d", done);
     #10
-    $display("The output has gone to %b", done);
+    $display("27: The output has gone to %d", done);
     #10
-    $display("The output has gone to %b", done);
+    $display("28: The output has gone to %d", done);
     #10
-    $display("The output has gone to %b", done);
+    $display("29: The output has gone to %d", done);
     #10
-    $display("The output has gone to %b", done);
+    $display("30: The output has gone to %d", done);
     #10
-    $display("The output has gone to %b", done);
+    $display("31: The output has gone to %d", done);
     #10
-    $display("The output has gone to %b", done);
-    #10
-    $display("The output has gone to %b", done);
-    #10
-    $display("The output has gone to %b", done);
+    
     $stop;
     
   end
