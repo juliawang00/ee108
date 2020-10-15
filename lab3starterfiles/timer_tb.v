7a7ce7a0e2c2541a0430d8af48b07fe3b8a936f7
TristanAbarca <52977898+TristanAbarca@users.noreply.github.com>
2020-10-14 20:44:17 -0600

    Update timer_tb.v

--- lab3starterfiles/timer_tb.v
+++ lab3starterfiles/timer_tb.v
@@ -1,27 +1,43 @@
 // Code your testbench here
 // or browse Examples
-module time_tb (input clk);
-  
+module timer_tb ();
+  reg clk;
+  reg rst;
   reg[3:0] load_value;
-
   reg count_en;
-  
+  reg fast;
   wire q;
-
-  timer #() DUT(.clk(clk), .load_value(load_value), .count_en(count_en), .q(q));
-
-  //forever
-  //  begin
-  //    #5 clk = 1 ; 
-  //    #5 clk = 0 ;
-  //  end
+  timer #() DUT(.clk(clk), .rst(rst), .load_value(load_value), .count_en(count_en), .fast(fast), .q(q));
+  
+	initial 
+      forever begin
+        #5 clk = 1;
+        #5 clk = 0;
+      end 
   
  initial begin
-
-    load_value = 4'b1000; 
-  	count_en = 1'b1;
-  
-   #100 $display("Load Value: %04b, Load Value: %03d, OUT: %04b", load_value, count_en, q);
+   #10 rst = 0;
+   #10 rst = 1;
+   #10 rst = 0;
    
+   rst = 1;
+   load_value = 4'b0001; 
+   fast = 1'b1;
+   count_en = 1'b1;
+   
+   #100 $display("Load Value: %b, Fast: %b, count_en: %b, OUT: %d", load_value, fast, count_en, q); 
+   
+    #10
+   $display("The output has gone to %b", q);
+    #10
+   $display("The output has gone to %b", q);
+    #10
+   $display("The output has gone to %b", q);
+    #10
+   $display("The output has gone to %b", q);
+    #10
+   $display("The output has gone to %b", q);
+   
+   $stop;                         
  end 
 endmodule 
