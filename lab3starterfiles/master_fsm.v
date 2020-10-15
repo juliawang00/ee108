254166e76e0f3e5d16f84ca0d67d566816530c3c
juliawang00 <juliawang00@gmail.com>
2020-10-14 23:44:42 -0400

    Update master_fsm.v

--- lab3starterfiles/master_fsm.v
+++ lab3starterfiles/master_fsm.v
@@ -1,17 +1,25 @@
 module master_fsm (
-  input clk,
-  input rst,
-  input next,
-  input faster,
-  input slower
+  input faster, 
+  input slower, 
+  input [3:0] next 
 );
-  programmable_blinker #(22) flash(.up_button(faster), .down_button(faster));
-  // state 1: OFF (also come here at reset)
+  wire [3:0] state; // total of 6 possible states
   
-  // state 2: ON
-  // state 3: OFF
-  // state 4: FLASH 1
-  // state 5: OFF
-  // state 6: FLASH 2
+  programmable_blinker #(22) flash(.up_button(faster), .down_button(faster));
+  always @(*) begin
+    case(state)
+      3'b001: // ON --> call mux with high
+        mux #() light(.on(1));
+      3'b011: // FLASH 1
+        programmable_blinker #() fast_blinker(.up_button(faster), .down_button(slower));
+      3'b101: // FLASH 2
+        programmable_blinker #() fast_blinker(.up_button(faster), .down_button(slower));
+      default: // OFF --> call mux with low
+        mux #() light(.on(0));
+    endcase
+  end
+  
+  assign state = next;
+  assign next = rst ? 3'b000 : next + 1;
 
 endmodule
