d605c24a21bded4775f8ee31c689c5c25e0b5b47
juliawang00 <juliawang00@gmail.com>
2020-10-15 00:52:06 -0400

    Update master_fsm.v

--- lab3starterfiles/master_fsm.v
+++ lab3starterfiles/master_fsm.v
@@ -1,24 +1,29 @@
 module master_fsm (
   input faster, 
   input slower, 
-  input [3:0] next 
+  input [3:0] next,
+  output wire mux_input,
+  output wire shift_left,
+  output wire shift_right
 );
   wire [3:0] state; // total of 6 possible states
   
   programmable_blinker #(22) flash(.up_button(faster), .down_button(faster));
+  reg input;
   always @(*) begin
     case(state)
       3'b001: // ON --> call mux with high
-        mux #() light(.on(1));
+        input = 1'b1;
       3'b011: // FLASH 1
         programmable_blinker #() fast_blinker(.up_button(faster), .down_button(slower));
       3'b101: // FLASH 2
-        programmable_blinker #() fast_blinker(.up_button(faster), .down_button(slower));
+        programmable_blinker #() slow_blinker(.up_button(faster), .down_button(slower));
       default: // OFF --> call mux with low
-        mux #() light(.on(0));
+        input = 1'b0;
     endcase
   end
   
+  assign mux_input = input;
   assign state = next;
   assign next = rst ? 3'b000 : next + 1;
 
