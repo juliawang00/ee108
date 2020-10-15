49cfedf562642b61ca519894c0550773a629639e
Troy Lawrence <35078371+tlaw72@users.noreply.github.com>
2020-10-15 05:55:49 +0200

    Final beat32.v

--- lab3starterfiles/beat32.v
+++ lab3starterfiles/beat32.v
@@ -1,45 +1,18 @@
 module beat32 #(parameter n = 22)
-  ( input clk, rst, load, 	// Clock, Reset, Value to be loaded into countdown: 22'd3125000
-    output wire done	// Output high when counter reaches zero
+  ( input clk, rst,  	// Clock, Reset, Value to be loaded into 
+   input [n-1:0] load,
+   output wire [n-1:0] done	// Output high when counter reaches zero
 );
   wire [n-1:0] next;
   wire [n-1:0] nextState;
   
+  // Initiate flip flop
   dffr #(n) count(clk, rst, nextState, next);
   
-  assign nextState = (next == 0) ? load : next - 1;
+  // Update the input state with load when output state is zero, otherwise -1
+  assign nextState = (next == 0) ? load -1 : next - 1;
 
+  // Output 1 if dffr output is zero
   assign done = (next == 0) ? 1 : 0;
-	//assign done = next;
   
 endmodule
-
-
-// module beat32 #(parameter n = 22)
-//   ( input clk, 
-//     input load, 	// Value to be loaded into countdown: 22'd3125000
-//     output wire done	// Output high when counter reaches zero
-// );
-//   wire [n-1:0] next, nextState;
-  
-//   dff #(n) count(clk, nextState, next);
-  
-//   Mux2 #(n) mux(next - 1'b1, load, {(|next), !(|next)}, nextState)
-  
-// //   // Counter: Decrement unless next is all zeros. Then set to load.
-// //   always @(*) begin
-// //     if (!(|next)) begin
-// //       nextState = load;
-// //     end
-// //     else if((|next)) begin
-// //       nextState = next - 1'b1;
-// //     end
-// //     else begin
-// //        nextState = next;
-// //     end
-// //   end
-  
-//   // Output high when counter reaches zero
-//   assign done = !(|next);
-
-// endmodule
