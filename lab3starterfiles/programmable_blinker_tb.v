4d1bb424cf650ccedb54630ba6029688ee8b420f
juliawang00 <juliawang00@gmail.com>
2020-10-14 21:18:07 -0400

    Update programmable_blinker_tb.v

--- lab3starterfiles/programmable_blinker_tb.v
+++ lab3starterfiles/programmable_blinker_tb.v
@@ -1,3 +1,18 @@
 module programmable_blinker_tb ();
-
-endmodule
+  reg up_button, down_button;
+  
+  programmable_blinker #() DUT(.up_button(up_button), .down_button(down_button));
+  
+  initial
+  forever
+   begin
+     #5 clk = 1 ; 
+     #5 clk = 0 ;
+   end
+  
+ initial begin
+   
+   $stop
+   
+ end 
+endmodule 
