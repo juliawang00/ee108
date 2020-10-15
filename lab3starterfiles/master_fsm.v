919dd4f67e5a115193de32590fe921fb972ed125
juliawang00 <juliawang00@gmail.com>
2020-10-14 21:18:02 -0400

    Update master_fsm.v

--- lab3starterfiles/master_fsm.v
+++ lab3starterfiles/master_fsm.v
@@ -1,6 +1,17 @@
 module master_fsm (
-
+  input clk,
+  input rst,
+  input next,
+  input faster,
+  input slower
 );
-
+  programmable_blinker #(22) flash(.up_button(faster), .down_button(faster));
+  // state 1: OFF (also come here at reset)
+  
+  // state 2: ON
+  // state 3: OFF
+  // state 4: FLASH 1
+  // state 5: OFF
+  // state 6: FLASH 2
 
 endmodule
