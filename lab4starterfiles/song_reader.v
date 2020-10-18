c50cf4da7f5d9ec52ab320d09c7838d58b7ef5fe
Troy Lawrence <35078371+tlaw72@users.noreply.github.com>
2020-10-18 09:54:57 -0700

    Quick first touches

--- lab4starterfiles/song_reader.v
+++ lab4starterfiles/song_reader.v
@@ -9,6 +9,20 @@ module song_reader(
     output [5:0] duration,
     output new_note
 );
+    reg [4:0] next;
+    reg [4:0] new_note;
+    
+    dffr #(5) clock(.clk(clk), ,r(reset), .d(next), .q(new_note));
+  
+    wire [5:0] addr = {song, };
+    wire [11:0] note;
+    song_rom #() get_notes(.clk(clk), .addr(), .dout(note));
+    
+    
+    
+    always@(*) begin
+        
+    end
 
     // Implementation goes here!
 
