0bbe959a6cd6cee1f0bf8abd7c01ddf16f5837e9
TristanAbarca <52977898+TristanAbarca@users.noreply.github.com>
2020-10-17 19:40:38 -0600

    Update sine_reader.v

--- lab4starterfiles/sine_reader.v
+++ lab4starterfiles/sine_reader.v
@@ -5,9 +5,30 @@ module sine_reader(
     input generate_next,
 
     output sample_ready,
-    output wire [15:0] sample
+  	output wire [15:0] sample
 );
-
-    //implementation goes here!
-
+  
+  	wire [19:0] current;
+  	wire [19:0] nextState;
+  
+  dffre #(20) count(.clk(clk), 
+                    .r(reset), 
+                    .en(generate_next), 
+                    .d(nextState), 
+                    .q(current));
+  
+    assign nextState = current + step_size;
+  
+  	wire [9:0] curr_add;
+  	assign curr_add = current >> 10;
+  	wire[15:0] curr_freq;
+  
+  sine_rom #() ROM(.clk(clk),
+                   .addr(curr_add),
+                   .dout(curr_freq));
+  
+  assign sample = curr_freq;
+  //figure out what im supposed to do for 4 qudrants ^^ only gets first
+  	
+  
 endmodule
