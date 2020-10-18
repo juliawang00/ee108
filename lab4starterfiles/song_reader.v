9801e5deb8b0878a06622aae0dfe4f447b541a4f
Troy Lawrence <35078371+tlaw72@users.noreply.github.com>
2020-10-18 12:30:37 -0700

    First draft. Incomplete

--- lab4starterfiles/song_reader.v
+++ lab4starterfiles/song_reader.v
@@ -9,18 +9,28 @@ module song_reader(
     output [5:0] duration,
     output new_note
 );
-    reg [4:0] next;
-    reg [4:0] new_note;
     
-    dffr #(5) clock(.clk(clk), ,r(reset), .d(next), .q(new_note));
+    // Implement a 32-second timer. Updates song_done if all the notes have been played and only increases when note_done is enabled
+    wire [4:0] next_state = reset ? 0 : new_note + 1;; 
+    wire [4:0] new_note;  
+    song_done = (next_state == 0 && new_note == 31) ? 1 : 0;
+    dffre #(5) clock(.clk(clk), ,r(reset), .en(note_done), .d(next_state), .q(new_note));
+    
   
-    wire [5:0] addr = {song, };
+    wire [6:0] addr = {song, new_note};
     wire [11:0] note;
-    song_rom #() get_notes(.clk(clk), .addr(), .dout(note));
+    song_rom #() get_notes(.clk(clk), .addr(addr), .dout(note));
+    
+    
+    note_player #() nt_play (.clk(clk), .reset(reset), .play_enable(), .node_to_load(note[11:5]), .duration_to_load(note[5:0]));
     
     
     
     always@(*) begin
+        case(rst)
+            
+        endcase
+        
         
     end
 
