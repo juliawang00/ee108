d29211f88b49a282880c9bac2f698a30110f7329
juliawang00 <juliawang00@gmail.com>
2020-10-19 18:03:46 -0400

    Update note_player.v

--- lab4starterfiles/note_player.v
+++ lab4starterfiles/note_player.v
@@ -11,7 +11,28 @@ module note_player(
     output [15:0] sample_out,  // Our sample output
     output new_sample_ready  // Tells the codec when we've got a sample
 );
+    
+    // Define the states
+    'define INITIAL_STATE  1'd0
+    'define LOOK_UP 1'd0
+    'define SINE_WAVE 1'd0
+    'define PAUSED 1'd0
+    'define NOTE_COMPLETE 1'd0
+    
+    reg [1:0] next_state, state;
+    dffre #(48) count(.clk(clk), .r(reset), .en(play_enable), .d(next_state), .q(state));
+    
+    reg [19:0] step_size;
+    frequency_rom #() ROM(.clk(clk), .addr(note_to_load), .dout(step_size));
+    
+    reg should_generate, sample_ready;
+    wire [15:0] sample; // holds sine wave generated
+    sine_reader #() SIN(.clk(clk), .reset(reset), .step_size(step_size), .generate_next(should_generate), .sample_ready(sample_ready), .sample(sample));
 
-    // Implementation goes here!
+    always @(*) begin
+        case(state)
+            
+        endcase
+    end
 
 endmodule
