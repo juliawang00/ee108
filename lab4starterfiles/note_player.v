4904d75ec9f08e228b045c9de1988c5fa9c437fa
juliawang00 <juliawang00@gmail.com>
2020-10-19 19:33:10 -0400

    Update note_player.v

--- lab4starterfiles/note_player.v
+++ lab4starterfiles/note_player.v
@@ -18,20 +18,40 @@ module note_player(
     'define SINE_WAVE 1'd0
     'define PAUSED 1'd0
     'define NOTE_COMPLETE 1'd0
+    'define PLAYING_NOTE 1'd0
     
     reg [1:0] next_state, state;
-    dffre #(48) count(.clk(clk), .r(reset), .en(play_enable), .d(next_state), .q(state));
+    dffre #(2) count(.clk(clk), .r(reset), .en(play_enable), .d(next_state), .q(state));
+    dffre #(6) duration(.clk(clk), .r(reset), .en(play_enable), .d(next_state), .q(state));
     
     reg [19:0] step_size;
     frequency_rom #() ROM(.clk(clk), .addr(note_to_load), .dout(step_size));
     
-    reg should_generate, sample_ready;
+    reg should_generate = load_new_note || generate_next_sample;
+    reg sample_ready;
     wire [15:0] sample; // holds sine wave generated
     sine_reader #() SIN(.clk(clk), .reset(reset), .step_size(step_size), .generate_next(should_generate), .sample_ready(sample_ready), .sample(sample));
 
     always @(*) begin
         case(state)
-            
+            'INITIAL_STATE: begin
+                // set intial values
+            end
+            'LOOK_UP: begin
+                // activitate and wait for the step_size lookup
+            end
+            'SINE_WAVE: begin
+                // activate and wait for the sine wave synthesis
+            end
+            'PAUSED: begin
+                // if we are in pause, wait for the play enable to go again
+            end
+            'PLAYING_NOTE begin
+                // increment counter for duration
+            end
+            'NOTE_COMPLETE: begin
+                // tell all modules the current note has finished
+            end
         endcase
     end
 
