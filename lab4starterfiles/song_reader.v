a9c678aca12ee74da9ab92275eca4c49aaec8d8e
Troy Lawrence <35078371+tlaw72@users.noreply.github.com>
2020-10-19 22:30:03 -0700

    State Mux implemented

--- lab4starterfiles/song_reader.v
+++ lab4starterfiles/song_reader.v
@@ -10,50 +10,86 @@ module song_reader(
     output new_note
 );
     
-    // Implement a 32-second timer. Updates song_done if all the notes have been played and only increases when note_done is enabled
-    wire [4:0] next_state = reset ? 5'b0 : state + 1;
-    wire [4:0] state;  
-    assign song_done = (note_done && next_state == 0 && state == 31) ? 1 : 0;
-    dffre #(5) clock(.clk(clk), .r(reset), .en(note_done), .d(next_state), .q(state)); 
     
-    // Instantiate two one-pulses to count one cycle after incrementing and after reading from ROM.
-    wire added;
-    wire ROM_read;
-    one_pulse #() first_puls(.clk(clk), .reset(reset), .in(note_done), .out(added));
-    one_pulse #() second_puls(.clk(clk), .reset(reset), .in(added), .out(ROM_read));
+    
+    // *** SONG_READER FSM ***
+    `define RESET 2'b00
+    `define INCREMENTED 2'b01
+    `define ROM_OUT 2'b10
+    `define CHECK_DONE 2'b11
+    
+    // State register
+    reg [1:0] next_state_d;
+    wire [1:0] current_state_q;
+
+    dffr #(2) state(
+        .clk(clk),
+        .d(next_state_d),
+        .q(current_state_q),
+        .r(reset)
+    );
+    
+    // Implement a 32-second timer. Updates song_done if all the notes have been played and only increases when note_done is enabled
+    reg [4:0] n_state;
+    wire [4:0] state;  
+    dffre #(5) clock(.clk(clk), .r(reset), .d(n_state), .q(state)); 
    
     
-    
+    // Instantiate ROM module.
     wire [6:0] addr = {song, state};
-    wire [11:0] past_note;
     wire [11:0] returned_note;
     song_rom #() get_notes(.clk(clk), .addr(addr), .dout(returned_note));
     
-    dffre #(5) clock(.clk(clk), .r(reset), .en(added), .d(returned_note), .q(past_note));
-    
-    assign {note, duration} = returned_note;
-    
-    always @(*) begin
-        case(ROM_read) begin
-            1'b0: {new_note, note, duration} = {1'b0, past_note};
-            1'b1: {new_note, note, duration} = {1'b1, returned_note};
-            default: {new_note, note, duration} = 13'b0;    
-        endcase
-    end
-    
-    
-    //note_player #() nt_play (.clk(clk), .reset(reset), .play_enable(), .node_to_load(note[11:5]), .duration_to_load(note[5:0]));    
-    
-//     always@(*) begin
-//         if(not_new_note != 0) begin
-//             assign notes, duration, new_note
-//         case(interim_note) begin // Case == Mux
-//             11'b0:  
-//             fff: note = interim_note[11:5]
-//             dddd: duration = interim_note[5:0]
-            
-//         endcase 
-//     end
 
+    // Assign new note values if in check_done state, otherwise keep the values the same and set new_note to zero.
+    assign {new_note, note, duration} = (current_state_q == `CHECK_DONE) ? {1'b1, returned_note} : {1'b0, note, duration}; 
+    
+    // Next state logic
+    always @(*)
+        case (current_state_q)
+            // RESET: When the reset button is pressed, the increment clock starts at 0. This is also the wait state
+            // in case pause is pressed. In this case, the increment clock retains its last value. In both cases,
+            // we only proceed to the next state if play is pressed.
+            `RESET : begin
+                n_state = reset ? 5'b0 : n_state; // Reset if reset button is on, otherwise keep with previous state. Necessary for pausing and playing.
+                if(play)
+                    next_state_d = `INCREMENTED;
+                else
+                    next_state_d = `RESET;
+            end
+            
+            // When play is pressed, we come to this state to represent that a cycle has passed and now state holds n_state value. Move to ROM_OUT no matter what.
+            `INCREMENTED : begin
+                next_state_d = `ROM_OUT;
+                
+            end
+            
+            // This state represents the cycle it takes to read from the ROM. At the end, 
+            `ROM_OUT : begin
+                next_state_d = `CHECK_DONE;
+                
+            end
+            
+            //
+            `CHECK_DONE : begin
+                n_state = state + 1; // Always increment one
+                if(note_done && play && n_state != 0) //Maybe state != 31
+                    next_state_d = `INCREMENTED;
+                else
+                    if (n_state == 0)
+                        song_done = 1;
+                    else
+                        song_done = 0;
+                    next_state_d = `WAIT;  
+            end
+            
+            //
+            default : begin
+                next_state_d = `RESET;
+                n_state = reset ? 5'b0 : n_state;
+                
+            end        
+                        
+        endcase
 
 endmodule
