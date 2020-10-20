297877cc811a9ca92f680420c1fb3f161ffe94f7
Troy Lawrence <35078371+tlaw72@users.noreply.github.com>
2020-10-19 17:16:16 -0700

    Some thoughts

--- lab4starterfiles/song_reader.v
+++ lab4starterfiles/song_reader.v
@@ -11,30 +11,38 @@ module song_reader(
 );
     
     // Implement a 32-second timer. Updates song_done if all the notes have been played and only increases when note_done is enabled
-    wire [4:0] next_state = reset ? 0 : new_note + 1;; 
-    wire [4:0] new_note;  
-    song_done = (next_state == 0 && new_note == 31) ? 1 : 0;
-    dffre #(5) clock(.clk(clk), ,r(reset), .en(note_done), .d(next_state), .q(new_note));
-    
-  
-    wire [6:0] addr = {song, new_note};
-    wire [11:0] note;
-    song_rom #() get_notes(.clk(clk), .addr(addr), .dout(note));
+    wire [4:0] next_state = reset ? 5'b0 : state + 1;
+    wire [4:0] state;  
+    assign song_done = (next_state == 0 && state == 31) ? 1 : 0;
+    dffre #(5) clock(.clk(clk), .r(reset), .en(note_done), .d(next_state), .q(state)); // Not sure if I want enabler, because that would take a few cycles to compute next value.
     
     
-    note_player #() nt_play (.clk(clk), .reset(reset), .play_enable(), .node_to_load(note[11:5]), .duration_to_load(note[5:0]));
+//     one_pulse() on_puls(.clk(clk), .reset(reset), .in(), .out());
     
+    wire [6:0] addr = {song, state};
+    wire [11:0] returned_note;
+    song_rom #() get_notes(.clk(clk), .addr(addr), .dout(returned_note));
     
-    
-    always@(*) begin
-        case(rst)
+    always @(*) begin
+        case(note_done) begin
+            1'b0: 
             
         endcase
-        
-        
+    end
+    
+    
+    //note_player #() nt_play (.clk(clk), .reset(reset), .play_enable(), .node_to_load(note[11:5]), .duration_to_load(note[5:0]));    
+    
+    always@(*) begin
+        if(not_new_note != 0) begin
+            assign notes, duration, new_note
+        case(interim_note) begin // Case == Mux
+            11'b0:  
+            fff: note = interim_note[11:5]
+            dddd: duration = interim_note[5:0]
+            
+        endcase 
     end
 
-    // Implementation goes here!
 
 endmodule
-
