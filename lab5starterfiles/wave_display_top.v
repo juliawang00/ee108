7a604e97d4f2dcaea3e91a5bfa23ee18d4386402
Troy Lawrence <35078371+tlaw72@users.noreply.github.com>
2020-10-28 15:49:50 -0700

    wave_capture module implemented

--- lab5starterfiles/wave_display_top.v
+++ lab5starterfiles/wave_display_top.v
@@ -12,5 +12,25 @@ module wave_display_top(
     output [7:0] b
 );
 
-// Implement me!
+    // Implement the wave_capture module
+    wire [8:0] write_address;
+    wire write_enable;
+    wire [7:0] write_sample;
+    wire read_index;
+    
+    wave_capture #() w_c (
+        .clk(clk),
+        .reset(reset),
+        .new_sample_ready(new_sample),
+        .new_sample_in(sample),
+        .wave_display_idle(~vsync), // The vsync goes low when done reading a frame, which is when we want the idle to go high.
+        .write_address(write_address),
+        .write_enable(write_enable),
+        .write_sample(write_sample),
+        .read_index(read_index)    
+    );
+    
+    
+    
+    
 endmodule
