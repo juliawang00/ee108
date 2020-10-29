dd8f84a331708a40de18455344e7e05c28d8479b
juliawang00 <juliawang00@gmail.com>
2020-10-29 01:59:37 -0400

    Update wave_display.v

--- lab5starterfiles/wave_display.v
+++ lab5starterfiles/wave_display.v
@@ -16,7 +16,6 @@ module wave_display (
     
     wire[6:0] read_value_adjusted = (read_value >> 1) + 6'd32; // New value for read_value to fit our small screen.
     assign valid_pixel = ((x[9] ^ x[8] == 1) && y[9] == 0); // Pixel is valid if (x is in quadrants 1 or 2) and y MSB is 0.
-    assign {r,g,b} = 24'hFFFFFF; // Make out output colors simply white for the time being.
     
     wire [7:0] prev_x; // holds value of previous x position on VGA display
     
