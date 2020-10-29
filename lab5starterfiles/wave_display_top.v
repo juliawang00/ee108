d3f5141de6ba28311a3ddf42d23de0e1e7a4c6b7
Troy Lawrence <35078371+tlaw72@users.noreply.github.com>
2020-10-28 23:02:26 -0700

    First attempt of wave_display_top.v

--- lab5starterfiles/wave_display_top.v
+++ lab5starterfiles/wave_display_top.v
@@ -12,13 +12,13 @@ module wave_display_top(
     output [7:0] b
 );
 
-    // Implement the wave_capture module
+    // Instantiate the wave_capture module
     wire [8:0] write_address;
     wire write_enable;
     wire [7:0] write_sample;
     wire read_index;
     
-    wave_capture #() w_c (
+    wave_capture #() w_c ( //FILL IN THE #() PARAM
         .clk(clk),
         .reset(reset),
         .new_sample_ready(new_sample),
@@ -31,6 +31,43 @@ module wave_display_top(
     );
     
     
+     // Instantiate the wave_display module
+    wire [8:0] read_address,
+    wire [7:0] read_value,
+    wire valid_pixel,
+    
+    wave_display #() w_d ( //FILL IN THE #() PARAM
+        .clk(clk),
+        .reset(reset),
+        .x(x),  // [0..1279]
+        .y(y),  // [0..1023]
+        .valid(valid),
+        .read_value(read_value),
+        .read_index(read_index),
+        .read_address(read_address),
+        .valid_pixel(valid_pixel),
+        .r(r),
+        .g(g),
+        .b(b)
+    );
+    
+    
+    // Instantiate the RAM module
+    wire[7:0] douta;
+    ram_1w2r #(8, 8) ram (
+    /* Dual read/write port A */
+        .clka(clk),
+        .wea(write_enable),
+        .addra(write_address),
+        .dina(write_sample),
+        .douta(douta),
+    
+    /* Read-only port B */
+        .clkb(clk),
+        .addrb(read_address),
+        .doutb(read_value)
+);
+       
     
     
 endmodule
