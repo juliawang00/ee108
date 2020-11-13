29dbd04ac6c32e40f093660e0f32f83541feb2ad
Troy Lawrence <35078371+tlaw72@users.noreply.github.com>
2020-11-13 12:38:52 -0800

    First crack with arbiter. Reduced from four read states to three.

--- final_project/song_reader.v
+++ final_project/song_reader.v
@@ -40,18 +40,8 @@ module song_reader(
     wire [`SONG_WIDTH-1:0] curr_note_num, next_note_num;    // Counter for addresses of a song. Up to 128.
     wire [`MSB + `NOTE_WIDTH + `DURATION_WIDTH + `OPTIONS -1:0] note_and_duration; // 16 bits. Value read from the rom and as specified in lab directions. MSB specifies if reading a note or wait period.
     wire [`SONG_WIDTH + 1:0] rom_addr = {song, curr_note_num}; // Address in the rom to read.
-    
-    
-    //note_done is ready to load. As soon as we load with arbiter, set that new_note high {new_note1, not1, duration1} = {1, 
-//     wire loaded1 = blah; 
-//     wire loaded2 = blah;
-//     wire loaded3 = blah;
-    
-//     wire availabe1 = note_done1 || ~loaded1; // HOW DO I FIGURE OUT IF A NOTE_PLAYER IS LOADED???
-//     wire availabe2 = note_done2 || ~loaded2;
-//     wire availabe3 = note_done3 || ~loaded3;
-    
-    //reg [47:0] all_notes_duration; // This wire dedicates sixteen bits for a note and duration to send to each note_player module. The module only expects 12 bits, but 16 makes computation easier.
+    wire [2:0] notes_done = {note_done1, note_done2, note_done3}; // Used in arbiter to see which note_player modules are available.
+    reg [2:0] player_available;
 
     wire [`SWIDTH-1:0] state;
     reg  [`SWIDTH-1:0] next;
@@ -87,8 +77,42 @@ module song_reader(
     
     // Count down when we are in the WAIT state, otherwise set the note to the duration value. It will be properly set before we reach state WAIT.
     assign duration_to_load = (state==`WAIT) ? countdown - 1 : note_and_duration[5:0];
-    wire done_wait = (countdown==6'b0);
+    wire done_wait = (countdown==6'b0);            
+
+    always @(*) begin
+        case (state)
+            `PAUSED:            next = play ? `RETRIEVE_NOTE1 : `PAUSED;
+            // HAVE ONLY ONE RETRIEVE STATE THAT GOES TO WAIT WHEN ARBITER OUTPUTS 0
+            `RETRIEVE_NOTE:    next = !play ? `PAUSED : note_and_duration[15] ? `WAIT : `RETRIEVE_NOTE;
+//             `RETRIEVE_NOTE2:    next = !play ? `PAUSED : note_and_duration[15] ? `WAIT : `RETRIEVE_NOTE3;
+//             `RETRIEVE_NOTE3:    next = !play ? `PAUSED : note_and_duration[15] ? `WAIT : `RETRIEVE_WAIT;
+//             `RETRIEVE_WAIT:     next = play ? `NEW_NOTE_READY : `PAUSED;
+//             `NEW_NOTE_READY:    next = play ? `WAIT: `PAUSED;
+            `WAIT:              next = !play ? `PAUSED
+                                            : (done_wait ? `INCREMENT_ADDRESS
+                                                          : `WAIT);
+            `INCREMENT_ADDRESS: next = (play && ~overflow) ? `RETRIEVE_NOTE
+                                                           : `PAUSED;
+            default:            next = `PAUSED;
+        endcase
+    end
+
+    // We increment when we are in one of the retrieve_note states with the MSB high, or if we are in increment address.
+    assign {overflow, next_note_num} =
+        ((state == `INCREMENT_ADDRESS) || ((state == `RETRIEVE_NOTE) && ~note_and_duration[15])) ? {1'b0, curr_note_num} + 1 : {1'b0, curr_note_num};
+
+    open_player arbiter(.bit_i(notes_done), .bit_o(player_available));
+    assign {new_note1, note1, duration1} = ((state == `RETRIEVE_NOTE) && ~note_and_duration[15] && player_available == 3'b100) ? {1'b1, note_and_duration[14:3]} : {13'b0};
+    assign {new_note2, note2, duration2} = ((state == `RETRIEVE_NOTE) && ~note_and_duration[15] && player_available == 3'b010) ? {1'b1, note_and_duration[14:3]} : {13'b0};
+    assign {new_note3, note3, duration3} = ((state == `RETRIEVE_NOTE) && ~note_and_duration[15] && player_available == 3'b001) ? {1'b1, note_and_duration[14:3]} : {13'b0};
     
+    assign song_done = overflow;
+
+endmodule
+
+// OLD CODE:
+
+
     // When the MSB of note_and_duration is high, we are at a wait period and should reset the counter otherwise we increment to load the next note_player_module
 //     assign next_note = note_and_duration[15] ? 2'b00 : cur_note + 1'b1; 
     
@@ -101,30 +125,7 @@ module song_reader(
 //             default:    all_notes_duration = {48'b0};
 //         endcase
 //     end
-            
 
-    always @(*) begin
-        case (state)
-            `PAUSED:            next = play ? `RETRIEVE_NOTE1 : `PAUSED;
-            // HAVE ONLY ONE RETRIEVE STATE THAT GOES TO WAIT WHEN ARBITER OUTPUTS 0
-            `RETRIEVE_NOTE1:    next = !play ? `PAUSED : note_and_duration[15] ? `WAIT : `RETRIEVE_NOTE2;
-            `RETRIEVE_NOTE2:    next = !play ? `PAUSED : note_and_duration[15] ? `WAIT : `RETRIEVE_NOTE3;
-            `RETRIEVE_NOTE3:    next = !play ? `PAUSED : note_and_duration[15] ? `WAIT : `RETRIEVE_WAIT;
-            `RETRIEVE_WAIT:     next = play ? `NEW_NOTE_READY : `PAUSED;
-            `NEW_NOTE_READY:    next = play ? `WAIT: `PAUSED;
-            `WAIT:              next = !play ? `PAUSED
-                                            : (done_wait ? `INCREMENT_ADDRESS
-                                                          : `WAIT);
-            `INCREMENT_ADDRESS: next = (play && ~overflow) ? `RETRIEVE_NOTE
-                                                           : `PAUSED;
-            default:            next = `PAUSED;
-        endcase
-    end
-
-    // We increment when we are in one of the retrieve_note states with the MSB high, or if we are in increment address.
-    assign {overflow, next_note_num} =
-        ((state == `INCREMENT_ADDRESS) || ((state == `RETRIEVE_NOTE1 || state == `RETRIEVE_NOTE2 || state == `RETRIEVE_NOTE3) && ~note_and_duration[15])) ? {1'b0, curr_note_num} + 1
-        : {1'b0, curr_note_num};
     
     //     assign new_note = (state == `NEW_NOTE_READY); DO IN ARBITER
     
@@ -133,7 +134,16 @@ module song_reader(
 //     assign {note2, duration2} = reset ? 12'b0 : ~loaded2 ? note_and_duration : {note2, duration2};
 //     assign {note3, duration3} = reset ? 12'b0 : ~loaded3 ? note_and_duration : {note3, duration3};
     
-    
-    assign song_done = overflow;
 
-endmodule
+    
+    //note_done is ready to load. As soon as we load with arbiter, set that new_note high {new_note1, not1, duration1} = {1, 
+//     wire loaded1 = blah; 
+//     wire loaded2 = blah;
+//     wire loaded3 = blah;
+    
+//     wire availabe1 = note_done1 || ~loaded1; // HOW DO I FIGURE OUT IF A NOTE_PLAYER IS LOADED???
+//     wire availabe2 = note_done2 || ~loaded2;
+//     wire availabe3 = note_done3 || ~loaded3;
+    
+    //reg [47:0] all_notes_duration; // This wire dedicates sixteen bits for a note and duration to send to each note_player module. The module only expects 12 bits, but 16 makes computation easier.
+
