2f1f69fb0bc61ea872fa30dab4442b43dc465b76
juliawang00 <juliawang00@gmail.com>
2020-11-19 17:17:40 -0500

    Create combiner_echo.v

---
+++ final_project/combiner_echo.v
@@ -0,0 +1,13 @@
+module combiner(
+  input [15:0] song_one, 
+  input [15:0] song_two,
+  input [15:0] song_three,
+
+  output [15:0] out
+);
+  
+  wire [17:0] added = (song_one + song_two + song_three);
+  
+  assign out = added >> ((added[17]) ? 2 : (added[16]) ? 1 : 0);
+ 
+endmodule
