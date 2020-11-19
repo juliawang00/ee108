2bb80c43c84ef525efa5e7724f9182f4863f15d6
juliawang00 <juliawang00@gmail.com>
2020-11-19 17:50:41 -0500

    Update combiner_echo_tb.v

--- final_project/combiner_echo_tb.v
+++ final_project/combiner_echo_tb.v
@@ -1,7 +1,8 @@
 module combiner_tb();
   
   reg [15:0] one, two, three;
-  wire[15:0] out;
+  reg [17:0] delay;
+  wire[17:0] out;
   
   combiner reader(
     .song_one(one),
@@ -15,51 +16,58 @@ module combiner_tb();
     one = 16'd10;
     two = 16'd20;
     three = 16'd30;
+    delay = 18'b0;
    
     #1
-    $display("%d + %d + %d = %d", one, two, three, out);
+    $display("%d + %d + %d + %d = %d", one, two, three, delay, out);
    	
+    delay = (one + two + three) >> 2;
     one = 16'd20;
     two = 16'd20;
     three = 16'd20;
    
     #1
-    $display("%d + %d + %d = %d", one, two, three, out);
+    $display("%d + %d + %d + %d = %d", one, two, three, delay, out);
     
+    delay = (one + two + three) >> 2;
     one = 16'd900;
     two = 16'd50;
     three = 16'd437;
    
     #1
-    $display("%d + %d + %d = %d", one, two, three, out);
+    $display("%d + %d + %d + %d = %d", one, two, three, delay, out);
     
+    delay = (one + two + three) >> 2;
     one = 16'd65535;
     two = 16'd1;
     three = 16'd0;
    
     #1
-    $display("%d + %d + %d = %d", one, two, three, out);
+    $display("%d + %d + %d + %d = %d", one, two, three, delay, out);
     
+    delay = (one + two + three) >> 2;
     one = 16'd65535;
     two = 16'd1;
     three = 16'd65535;
    
     #1
-    $display("%d + %d + %d = %d", one, two, three, out);
+    $display("%d + %d + %d + %d = %d", one, two, three, delay, out);
     
+    delay = (one + two + three) >> 2;
     one = 16'd65535;
     two = 16'd2;
     three = 16'd65535;
    
     #1
-    $display("%d + %d + %d = %d", one, two, three, out);
+    $display("%d + %d + %d + %d = %d", one, two, three, delay, out);
     
+    delay = (one + two + three) >> 2;
     one = 16'd65535;
     two = 16'd65535;
     three = 16'd65535;
    
     #1
-    $display("%d + %d + %d = %d", one, two, three, out);
+    $display("%d + %d + %d + %d = %d", one, two, three, delay, out);
     
   end
   
