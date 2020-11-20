d74b0693ad252af98b888e83c91fd92ead2a4483
Troy Lawrence <35078371+tlaw72@users.noreply.github.com>
2020-11-20 00:39:37 -0700

    Update combiner.v

--- final_project/combiner.v
+++ final_project/combiner.v
@@ -8,6 +8,6 @@ module combiner(
   
   wire [17:0] added = (song_one + song_two + song_three);
   
-  assign out = (added >> ((added[17]) ? 2 : (added[16]) ? 1 : 0))[0:15];
+  assign out = added[17] ? added[17:2] : added[16] ? added[16:1] : added[15:0];
  
 endmodule
