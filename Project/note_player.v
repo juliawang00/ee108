38b1499b4cc1ae4b59c234a7ac0ffef70ecf9d1b
juliawang00 <juliawang00@gmail.com>
2020-11-20 01:12:09 -0500

    Update note_player.v

--- Project/note_player.v
+++ Project/note_player.v
@@ -1,44 +1,54 @@
-module sine_reader(
+module note_player(
     input clk,
     input reset,
-    input [19:0] step_size,
-    input generate_next,
-
-    output sample_ready,
-    output wire [15:0] sample
+    input play_enable,  // When high we play, when low we don't.
+    input [5:0] note_to_load,  // The note to play
+    input [5:0] duration_to_load,  // The duration of the note to play
+    input load_new_note,  // Tells us when we have a new note to load
+    output done_with_note,  // When we are done with the note this stays high.
+    input beat,  // This is our 1/48th second beat
+    input generate_next_sample,  // Tells us when the codec wants a new sample
+    output [15:0] sample_out,  // Our sample output
+    output new_sample_ready  // Tells the codec when we've got a sample
 );
 
-    //states
-    localparam SWIDTH = 22;
+    wire [19:0] step_size;
+    wire [5:0] freq_rom_in;
 
-    wire [SWIDTH - 1:0] addr;
-    dffre #(.WIDTH(SWIDTH)) state_reg (
+    dffre #(.WIDTH(6)) freq_reg (
         .clk(clk),
         .r(reset),
-        .en(generate_next),
-        .d(addr + step_size),
-        .q(addr)
+        .en(load_new_note),
+        .d(note_to_load),
+        .q(freq_rom_in)
     );
 
-    wire [9:0] sine_rom_addr = addr[20] ? ~addr[19:10] : addr[19:10];
-    wire [15:0] sine_rom_out;
-    sine_rom sineRom (
+    frequency_rom freq_rom(
         .clk(clk),
-        .addr(sine_rom_addr),
-        .dout(sine_rom_out)
+        .addr(freq_rom_in),
+        .dout(step_size)
     );
-    assign sample = addr[21] ? -sine_rom_out : sine_rom_out;
 
-    wire almost_ready;
-    dff #(.WIDTH(1)) ready_1 (
+    sine_reader sine_read(
         .clk(clk),
-        .d(generate_next),
-        .q(almost_ready)
+        .reset(reset),
+        .step_size(step_size),
+        .generate_next(play_enable && generate_next_sample),
+        .sample_ready(new_sample_ready),
+        .sample(sample_out)
     );
-    dff #(.WIDTH(1)) ready_2 (
+
+    wire [5:0] state, next_state;
+    dffre #(.WIDTH(6)) state_reg (
         .clk(clk),
-        .d(almost_ready),
-        .q(sample_ready)
+        .r(reset),
+        .en((beat || load_new_note) && play_enable),
+        .d(next_state),
+        .q(state)
     );
+    assign next_state = (reset || done_with_note || load_new_note)
+                        ? duration_to_load : state - 1;
+
+    assign done_with_note = (state == 6'b0) && beat;
 
 endmodule
