c45f4c7a1294f5fb4bb0e1aae50abbd032a48fff
Troy Lawrence <35078371+tlaw72@users.noreply.github.com>
2020-11-20 13:54:31 -0700

    Update song_reader.v

--- final_project/song_reader.v
+++ final_project/song_reader.v
@@ -1,4 +1,4 @@
-`define SONG_WIDTH 8
+`define SONG_WIDTH 8 // 128bits
 `define NOTE_WIDTH 6
 `define DURATION_WIDTH 6
 `define MSB 1
@@ -26,7 +26,8 @@ module song_reader(
 );
     wire [`SONG_WIDTH-1:0] curr_note_num, next_note_num;    // Counter for addresses of a song. Up to 128.
     wire [`MSB + `NOTE_WIDTH + `DURATION_WIDTH + `OPTIONS -1:0] note_and_duration; // 16 bits. Value read from the rom and as specified in lab directions. MSB specifies if reading a note or wait period.
-    wire [`SONG_WIDTH - 2:0] rom_addr = {song, curr_note_num}; // Address in the rom to read.
+    //     wire [`SONG_WIDTH + 1:0] rom_addr = {song, curr_note_num}; // Address in the rom to read.
+    wire [`SONG_WIDTH - 1:0] rom_addr = curr_note_num; // Address in the rom to read.
     wire [2:0] notes_done = {note_done1, note_done2, note_done3}; // Used in arbiter to see which note_player modules are available.
     wire [2:0] player_available;
 
@@ -45,6 +46,7 @@ module song_reader(
         .d(next_note_num),
         .q(curr_note_num)
     );
+    
     dffr #(`SWIDTH) fsm (
         .clk(clk),
         .r(reset),
@@ -60,6 +62,7 @@ module song_reader(
         .q(countdown)
     );
 
+    // Get back the note and duration from the Song_Rom, including if it is a note or a hold period (as determined by MSB).
     song_rom rom(.clk(clk), .addr(rom_addr), .dout(note_and_duration));
     
     // Count down when we are in the WAIT state, otherwise set the note to the duration value. It will be properly set before we reach state WAIT.
@@ -82,7 +85,8 @@ module song_reader(
         endcase
     end
 
-    // We increment when we are in one of the retrieve_note states with the MSB high, or if we are in increment address.
+    // We increment when we are in one of the retrieve_note states with the MSB high, or if we are in increment address. Overflow is set high when the last note of 
+    // a song is played, eg {1'd0,8'd127} + 1 = {1'd1, 8'd0}
     assign {overflow, next_note_num} =
         ((state == `INCREMENT_ADDRESS) || ((state == `RETRIEVE_NOTE) && ~note_and_duration[15])) ? {1'b0, curr_note_num} + 1 : {1'b0, curr_note_num};
 
