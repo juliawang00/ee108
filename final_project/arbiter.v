910fcdba1385dae7fadfaa4212134bb124ab1770
Troy Lawrence <35078371+tlaw72@users.noreply.github.com>
2020-11-20 01:11:04 -0700

    Update arbiter.v

--- final_project/arbiter.v
+++ final_project/arbiter.v
@@ -3,18 +3,19 @@
 
 module arbiter(
     input  wire [2:0] bit_i,
-    output reg [2:0] bit_o
+    output wire [2:0] bit_o
     );
-    
+    reg[2:0] bit_x;
+    assign bit_o = bit_x;
     always @ (*) begin
         if (bit_i[2])
-          bit_o = 3'b100;
+          bit_x = 3'b100;
         else if (bit_i[1])
-          bit_o = 3'b010;
+          bit_x = 3'b010;
         else if (bit_i[0]) 
-          bit_o = 3'b001;
+          bit_x = 3'b001;
       else
-          bit_o = 3'b000;
+          bit_x = 3'b000;
     end
 
 endmodule
